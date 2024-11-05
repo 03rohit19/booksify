8f0f8dddb6b8695ba4b50509e78cbe439bacc445
Rohit <rohitfriday01@gmail.com>
2024-11-05 17:22:59 +0530

    Adding updated file

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import { Routes, Route } from "react-router-dom";
+// Components
+import NavbarPage from "./components/Navbar";
+
 // Pages
 import Register from "./pages/Register";
 import Login from "./pages/Login";
+import List from "./pages/List";
 
 // CSS
 import "./App.css";
@@ -9,11 +13,15 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<h1>Home</h1>} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-    </Routes>
+    <div>
+      <NavbarPage />
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/book/list" element={<List />} />
+      </Routes>
+    </div>
   );
 }
 
